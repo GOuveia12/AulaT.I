309686dcc5fd8ca285b0b3b1dd3375dab1611286
pedroxx21 <168564460+pedroxx21@users.noreply.github.com>
2024-06-17 16:50:53 -0300

    Add files via upload

---
+++ src/applicationintegrada/ApplicationIntegrada.java
@@ -0,0 +1,13 @@
+package applicationintegrada;
+import interfaceGrafica.telaLogin;
+/**
+ *
+ * @author gabriel.pagvelosa
+ */
+public class ApplicationIntegrada {
+    public static void main(String[] args) {
+        telaLogin l = new telaLogin();
+        l.setVisible(true);
+    }
+    
+}
